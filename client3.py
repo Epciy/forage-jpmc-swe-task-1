33e3875dbfcfd009b17152d71e46b6d642c72317
Epciy <amountii00@gmail.com>
2023-10-02 13:59:16 +0200

    task1

--- client3.py
+++ client3.py
@@ -32,18 +32,43 @@ N = 500
 def getDataPoint(quote):
     """ Produce all the needed values to generate a datapoint """
     """ ------------- Update this function ------------- """
-    stock = quote['stock']
-    bid_price = float(quote['top_bid']['price'])
-    ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    """try:
+    
+        stock = quote['stock']
+
+        
+        bid_price = float(quote['top_bid']['price'])
+        ask_price = float(quote['top_ask']['price'])
+        price = (bid_price+ask_price)//2
+        return stock, bid_price, ask_price, price"""
+    try:
+        stock = quote['stock']
+    except KeyError:
+        stock = None  
+
+    try:
+        bid_price = float(quote['top_bid']['price'])
+        ask_price = float(quote['top_ask']['price'])
+        price = (bid_price + ask_price) / 2
+    except (KeyError, ValueError):
+        bid_price = 0.0  
+        ask_price = 0.0
+        price = 0.0
+
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    try:
+        result=price_a/price_b
+        return result
 
+    
+    except ZeroDivisionError:
+            raise ValueError("Division by zero is not allowed")
+ 
 
 # Main
 if __name__ == "__main__":
@@ -52,8 +77,10 @@ if __name__ == "__main__":
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
         """ ----------- Update to get the ratio --------------- """
+        prices={}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
+            prices[stock]=price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+        print("Ratio %s" % getRatio(prices["ABC"], prices["DEF"]))
--- client_test.py
+++ client_test.py
@@ -8,6 +8,10 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    expected_result = ('ABC', 120.48, 121.2,120.84)
+    result = getDataPoint(quotes[0])
+    self.assertEqual(result, expected_result)
+
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,11 +19,22 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    expected_result = ('ABC', 120.48, 119.2, 119.84)
+    result = getDataPoint(quotes[0])
+    self.assertEqual(result, expected_result)
 
 
   """ ------------ Add more unit tests ------------ """
-
-
+  def test_getDataPoint_emptyQuote(self):
+    quotes = {}
+    expected_result = (None, 0.0, 0.0, 0.0)
+    result = getDataPoint(quotes)
+    self.assertEqual(result, expected_result) 
+  def test_getDataPoint_missingStockKey(self):
+    quote = {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}}
+    expected_result = (None, 120.48, 121.2, 120.84) 
+    result = getDataPoint(quote)
+    self.assertEqual(result, expected_result)
 
 if __name__ == '__main__':
     unittest.main()
